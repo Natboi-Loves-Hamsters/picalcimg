87aa3fc8fa2893e30d894d659295f1b54b2b0e08
Natboi-Loves-Hamsters <nathanlegoblox@gmail.com>
2023-11-22 12:59:21 -0700

    Made it so it can itterate through pi to find images

--- venv/main.py
+++ venv/main.py
@@ -1,8 +1,38 @@
+wrap = 4
+
 with open('pi100000.dat') as f:
     lpi = f.readlines()
 for pi in lpi:
     continue
 pi = pi[2:]
+
+palist = [[],[],[],[]]
+curarnum = 0
+ialist = [['0','1','1','1'],['1','1','0','0'],['1','1','1','1'],['0','1','0','1']]
+running = True
+
+while running:
+    if palist != ialist:
+        for digit in pi:
+            if len(palist[3]) <= (wrap-1):
+                palist[3].append(digit)
+            if len(palist[3]) == (wrap):
+                if len(palist[2]) != (wrap):
+                    palist[2] = palist[3]
+                    palist[3] = []
+                if len(palist[1]) != (wrap):
+                    palist[1] = palist[2]
+                    palist[2] = []
+                if len(palist[0]) != (wrap):
+                    palist[0] = palist[1]
+                    palist[1] = []
+            if ialist == palist:
+                print("YES")
+            elif len(palist[0]) == wrap and len(palist[1]) == wrap and len(palist[2]) == wrap and len(palist[3]) == wrap:
+                palist[0] = palist[1]
+                palist[1] = palist[2]
+                palist[2] = palist[3]
+                palist[3] = []
 
 vpi = ""
 for digit in pi:
@@ -10,5 +40,4 @@ for digit in pi:
         vpi += ("░")
     else:
         vpi += ("█")
-
 print(vpi)
