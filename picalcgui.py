18b0a7dc6562c67f2b1b9fd4b27563c4b95a1b39
natboi <nathanlegoblox@gmail.com>
2023-12-03 00:32:19 -0700

    bug fixes and finishing gui

--- picalcgui.py
+++ picalcgui.py
@@ -1,16 +1,22 @@
 from main import piGen, piSearch
 import PySimpleGUI as sg
 
+# ialist = ['1001','0000'],'1001','0110']
+ialist = ['0111', '1100', '1111', '0101']
+wrap = len(ialist)
+
+
 column1 = [
     [sg.Text('How many digits of pi?'),sg.InputText(size=(10))],
     [sg.Checkbox('Generate pi?',enable_events=True, key='initpigen')],
     [sg.Checkbox('Save pi?',enable_events=True, key='save')],
     [sg.Submit(key='submit')],
 ]
-layout = [[sg.Column(column1,element_justification='center'), sg.VSep(pad=(5)), sg.Output(key='vpigui'+sg.WRITE_ONLY_KEY, size=(40,8))]]
+layout = [[sg.Column(column1,element_justification='center'), sg.VSep(pad=(5)), sg.MLine(key='vpigui'+sg.WRITE_ONLY_KEY, size=(wrap * 2,8))]]
 
-print = lambda *args, **kwargs: window['vpigui'+sg.WRITE_ONLY_KEY].print(*args, **kwargs)
 window = sg.Window("piCalcGui", layout, margins=(10,10))
+
+print = lambda *args, **kwargs: window['vpigui' + sg.WRITE_ONLY_KEY].print(*args, **kwargs)
 
 initpigen = False
 save = False
@@ -25,16 +31,20 @@ while not sg.WINDOW_CLOSED:
                 exit()
             if event == 'initpigen':
                 initpigen = True
-            if event == 'initpigen' and event == 'save':
+            if event == 'save':
                 save = True
         DIGITS = values[0]
         DIGITS = int(DIGITS)
 
     pi = 3
 
-    if initpigen == True:
-        pi = piGen(DIGITS)
-        pi = pi[2:]
+    if initpigen:
+        if save:
+            pi = piGen(DIGITS, save=True)
+            pi = pi[2:]
+        else:
+            pi = piGen(DIGITS)
+            pi = pi[2:]
 
     else:
         # opens pi file and assigns it to pi
@@ -44,9 +54,7 @@ while not sg.WINDOW_CLOSED:
         pi = lpi[0]
         pi = pi[2:]
 
-    # ialist = ['1001','0000'],'1001','0110']
-    ialist = ['0111', '1100', '1111', '0101']
-    vpi = piSearch(pi,ialist)
+    num = 1
+    vpi = piSearch(pi,ialist, verbose=False)
 
-    layout = [sg.Multiline(key='vpi')]
     print(vpi)
