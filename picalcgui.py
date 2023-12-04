03dc5b3fe30bb0c2ab4af82646f0219e5dadde2a
Natboi-Loves-Hamsters <nathanlegoblox@gmail.com>
2023-12-04 13:17:34 -0700

    Fixes and gui is ready

--- picalcgui.py
+++ picalcgui.py
@@ -1,5 +1,8 @@
 from main import piGen, piSearch
 import PySimpleGUI as sg
+sg.theme('SystemDefaultForReal')
+
+# I NEED TO ADD A SEARCH FUNCTION
 
 # ialist = ['1001','0000'],'1001','0110']
 ialist = ['0111', '1100', '1111', '0101']
@@ -8,41 +11,47 @@ wrap = len(ialist)
 
 column1 = [
     [sg.Text('How many digits of pi?'),sg.InputText(size=(10))],
-    [sg.Checkbox('Generate pi?',enable_events=True, key='initpigen')],
+    [sg.Checkbox('Generate pi?',enable_events=True, key='initpigen', default=True)],
     [sg.Checkbox('Save pi?',enable_events=True, key='save')],
     [sg.Submit(key='submit')],
 ]
 layout = [[sg.Column(column1,element_justification='center'), sg.VSep(pad=(5)), sg.MLine(key='vpigui'+sg.WRITE_ONLY_KEY, size=(wrap * 2,8))]]
 
-window = sg.Window("piCalcGui", layout, margins=(10,10))
+window = sg.Window("piCalcGui", layout, margins=(10,10), icon="icon.ico")
 
 print = lambda *args, **kwargs: window['vpigui' + sg.WRITE_ONLY_KEY].print(*args, **kwargs)
 
-initpigen = False
+initpigen = True
 save = False
 
 while not sg.WINDOW_CLOSED:
+    event = False
     DIGITS = 0
-    while DIGITS < 2 or DIGITS == 1000:
-        event = False
+    while DIGITS < 2 or DIGITS == 1000 or DIGITS:
         while event != 'submit':
             event, values = window.read()
             if event == sg.WINDOW_CLOSED:
                 exit()
             if event == 'initpigen':
-                initpigen = True
+                initpigen = values['initpigen']
             if event == 'save':
-                save = True
+                save = values['save']
         DIGITS = values[0]
-        DIGITS = int(DIGITS)
+        try:
+            DIGITS = int(DIGITS)
+        except:
+            DIGITS = 0
+            event = False
 
     pi = 3
 
     if initpigen:
         if save:
+            print("Making and saving pi")
             pi = piGen(DIGITS, save=True)
             pi = pi[2:]
         else:
+            print("Making pi")
             pi = piGen(DIGITS)
             pi = pi[2:]
 
@@ -55,6 +64,8 @@ while not sg.WINDOW_CLOSED:
         pi = pi[2:]
 
     num = 1
+    print("Searching pi")
     vpi = piSearch(pi,ialist, verbose=False)
 
+    window['vpigui'+sg.WRITE_ONLY_KEY].Update('')
     print(vpi)
